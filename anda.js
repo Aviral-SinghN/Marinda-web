4a66b6b2613cf6dc3af19afba3165a949ec0b227
Aviral-SinghN <aviralsingh2721@gmail.com>
2024-11-11 11:19:41 +0530

    Update anda.js

---
+++ anda.js
@@ -0,0 +1,22 @@
+var tl = gsap.timeline()
+
+// tl.to(".rectangle",{
+//     y:"100vh",
+//     scale:0.6,
+//     duration:0
+// })
+tl.to(".rectangle",{
+    y:"40vh",
+    duration:0,
+    
+})
+// tl.to(".rectangle",{
+//     y:"0vh",
+//     rotate:360,
+//     scale:1,
+//     duration:0.7
+// })
+tl.to(".rectangle",{
+    y:"0vh",
+    duration:2,
+})
